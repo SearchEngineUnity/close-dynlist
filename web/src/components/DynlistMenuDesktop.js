877c1cf3bf10e3ad670bebd4736b8a39f5fb906c
sc-searchengineunity <sharon@searchengineunity.com>
2020-10-16 13:24:06 -0400

    updated to include active page state for dynlist desktop menu

--- web/src/components/DynlistMenuDesktop.js
+++ web/src/components/DynlistMenuDesktop.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { navigate } from 'gatsby';
+import { Location } from '@reach/router';
 import { ListGroup } from 'react-bootstrap';
 import styled from 'styled-components';
 
 const DesktopMenu = styled(ListGroup)`
-  display: flex;
   @media (max-width: 575.98px) {
     display: none;
   }
@@ -17,33 +17,43 @@ function DynlistMenuDesktop({ menu }) {
   };
 
   return (
-    <DesktopMenu>
-      {menu.map((item) => {
+    <Location>
+      {({ location }) => {
         return (
-          <React.Fragment key={item._key}>
-            <ListGroup.Item
-              action
-              onClick={() => onClickHandler(item.nav.slug.current)}
-              bsPrefix="list-group-item dynlist-navItem-tierOne--desktop text-left"
-              activeClassName="dynlist-navItem-tierOne--desktop-active"
-            >
-              {item.label}
-            </ListGroup.Item>
-            {item.group.map((el) => (
-              <ListGroup.Item
-                action
-                onClick={() => onClickHandler(el.nav.slug.current)}
-                bsPrefix="list-group-item dynlist-navItem-tierTwo--desktop text-left"
-                key={el._key}
-                activeClassName="dynlist-navItem-tierTwo--desktop-active"
-              >
-                {el.label}
-              </ListGroup.Item>
-            ))}
-          </React.Fragment>
+          <DesktopMenu>
+            {menu.map((item) => {
+              const isActiveCategorySet = `/${item.nav.slug.current}` === location.pathname;
+              return (
+                <React.Fragment key={item._key}>
+                  <ListGroup.Item
+                    action
+                    onClick={() => onClickHandler(item.nav.slug.current)}
+                    bsPrefix="dynlist-navItem-tierOne--desktop text-left"
+                    active={isActiveCategorySet}
+                  >
+                    {item.label}
+                  </ListGroup.Item>
+                  {item.group.map((el) => {
+                    const isActiveCategory = `/${el.nav.slug.current}` === location.pathname;
+                    return (
+                      <ListGroup.Item
+                        action
+                        onClick={() => onClickHandler(el.nav.slug.current)}
+                        bsPrefix="dynlist-navItem-tierTwo--desktop text-left"
+                        key={el._key}
+                        active={isActiveCategory}
+                      >
+                        {el.label}
+                      </ListGroup.Item>
+                    );
+                  })}
+                </React.Fragment>
+              );
+            })}
+          </DesktopMenu>
         );
-      })}
-    </DesktopMenu>
+      }}
+    </Location>
   );
 }
 

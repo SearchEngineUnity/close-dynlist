cd856b52fe2420ab1a400a80470d4636d8a78257
sc-searchengineunity <sharon@searchengineunity.com>
2020-10-27 16:32:35 -0400

    fix font related browser errors

--- web/src/containers/layout.js
+++ web/src/containers/layout.js
@@ -16,6 +16,18 @@ const Layout = ({ children }) => {
   return (
     <>
       <Helmet>
+        <link rel="preconnect" href="https://fonts.gstatic.com/" crossOrigin />
+        <link
+          rel="preload"
+          as="font"
+          type="font/woff2"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          crossOrigin
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
+          rel="stylesheet"
+        />
         <script
           rel="preload"
           src="https://kit.fontawesome.com/e4a269ffa7.js"
@@ -23,20 +35,18 @@ const Layout = ({ children }) => {
           async
         />
         <script />
-      </Helmet>
-      <MainNav />
-      <>{children}</>
-      <MainFooter />
-      <Helmet>
         <link
           rel="preload"
           href="https://close.com/static/dist/style.min.css?h=f48dda9a"
           as="style"
         />
-        <link rel="preload" type="text/css" href={withPrefix('globalStyle.css')} as="style" />
         <link rel="stylesheet" href="https://close.com/static/dist/style.min.css?h=f48dda9a" />
+        <link rel="preload" type="text/css" href={withPrefix('globalStyle.css')} as="style" />
         <link rel="stylesheet" type="text/css" href={withPrefix('globalStyle.css')} />
       </Helmet>
+      <MainNav />
+      <>{children}</>
+      <MainFooter />
     </>
   );
 };

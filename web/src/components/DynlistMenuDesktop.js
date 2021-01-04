e7c94c8566182a6a57e7ea3719df743b02f625cd
sc-searchengineunity <sharon@searchengineunity.com>
2021-01-04 13:44:25 -0500

    remove all unused code from the dynlist menu

--- web/src/components/DynlistMenuDesktop.js
+++ web/src/components/DynlistMenuDesktop.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link, navigate } from 'gatsby';
-import { Location } from '@reach/router';
+import { Link } from 'gatsby';
 import styled from 'styled-components';
 
 const DesktopMenu = styled.nav`
@@ -12,44 +11,35 @@ const DesktopMenu = styled.nav`
 
 function DynlistMenuDesktop({ menu }) {
   return (
-    <Location>
-      {({ location }) => {
+    <DesktopMenu className="list-group" aria-label="Navigation for quotes categories">
+      {menu.map((item) => {
         return (
-          <DesktopMenu className="list-group" aria-label="Navigation for quotes categories">
-            {menu.map((item) => {
-              const isActiveCategorySet = `/quotes/${item.nav.slug.current}/` === location.pathname;
+          <React.Fragment key={item._key}>
+            <Link
+              to={`/quotes/${item.nav.slug.current}/`}
+              // onClick={() => onClickHandler(item.nav.slug.current)}
+              className="dynlist-navItem-tierOne--desktop text-left"
+              activeClassName="active"
+            >
+              {item.label}
+            </Link>
+            {item.group.map((el) => {
               return (
-                <React.Fragment key={item._key}>
-                  <Link
-                    to={`/quotes/${item.nav.slug.current}/`}
-                    // onClick={() => onClickHandler(item.nav.slug.current)}
-                    className="dynlist-navItem-tierOne--desktop text-left"
-                    activeClassName="active"
-                  >
-                    {item.label}
-                  </Link>
-                  {item.group.map((el) => {
-                    const isActiveCategory =
-                      `/quotes/${el.nav.slug.current}/` === location.pathname;
-                    return (
-                      <Link
-                        to={`/quotes/${el.nav.slug.current}/`}
-                        // onClick={() => onClickHandler(el.nav.slug.current)}
-                        className="dynlist-navItem-tierTwo--desktop text-left"
-                        key={el._key}
-                        activeClassName="active"
-                      >
-                        {el.label}
-                      </Link>
-                    );
-                  })}
-                </React.Fragment>
+                <Link
+                  to={`/quotes/${el.nav.slug.current}/`}
+                  // onClick={() => onClickHandler(el.nav.slug.current)}
+                  className="dynlist-navItem-tierTwo--desktop text-left"
+                  key={el._key}
+                  activeClassName="active"
+                >
+                  {el.label}
+                </Link>
               );
             })}
-          </DesktopMenu>
+          </React.Fragment>
         );
-      }}
-    </Location>
+      })}
+    </DesktopMenu>
   );
 }
 
